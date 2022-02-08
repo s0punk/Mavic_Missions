ce8be9ddd6bcf8304cba6634b5b13cbfef200ce0
vais <simonoliviervaill@gmail.com>
2022-02-08 15:38:54 -0500

    Mise à jour du controller de mouvement.

--- app/src/main/java/com/vais/mavicmissions/services/AircraftController.java
+++ app/src/main/java/com/vais/mavicmissions/services/AircraftController.java
@@ -6,37 +6,12 @@ public class AircraftController {
     private float yaw;
     private float throttle;
 
-
-
-    public float getPitch() {
-        return pitch;
-    }
-
-    public void setPitch(float pitch) {
-        this.pitch = pitch;
-    }
-
-    public float getRoll() {
-        return roll;
-    }
-
-    public void setRoll(float roll) {
-        this.roll = roll;
-    }
-
-    public float getYaw() {
-        return yaw;
-    }
-
-    public void setYaw(float yaw) {
-        this.yaw = yaw;
-    }
-
-    public float getThrottle() {
-        return throttle;
-    }
-
-    public void setThrottle(float throttle) {
-        this.throttle = throttle;
-    }
+    public float getPitch() { return pitch; }
+    public void setPitch(float pitch) { this.pitch = pitch; }
+    public float getRoll() { return roll; }
+    public void setRoll(float roll) { this.roll = roll; }
+    public float getYaw() { return yaw; }
+    public void setYaw(float yaw) { this.yaw = yaw; }
+    public float getThrottle() { return throttle; }
+    public void setThrottle(float throttle) { this.throttle = throttle; }
 }
