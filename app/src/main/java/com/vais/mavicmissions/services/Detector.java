d9dc16f5cb5e05b954e26b50bb369e08d68cd2ba
vais <simonoliviervaill@gmail.com>
2022-03-28 13:02:10 -0400

    Détection de direction

--- app/src/main/java/com/vais/mavicmissions/services/Detector.java
+++ app/src/main/java/com/vais/mavicmissions/services/Detector.java
@@ -42,32 +42,8 @@ public class Detector {
 
     public static double detectDirection(Point[] corners) {
         double angle = 0;
-        Point c1 = null;
-        Point c2 = null;
-        double smallestDistance = 0;
 
-        // Trouver le point central, c1.
-
-
-        for (int i = 0; i < corners.length; i++) {
-            for (int j = 0; j < corners.length; j++) {
-                // Trouver la distance entre le point présent et le suivant (d=√((x_2-x_1)²+(y_2-y_1)²)).
-                double r1 = Math.pow(corners[i].x - corners[j].x, 2);
-                double r2 = Math.pow(corners[i].y - corners[j].y, 2);
-                double d = Math.sqrt(r1 + r2);
-
-                if (smallestDistance == 0 || d < smallestDistance && d != 0) {
-                    smallestDistance = d;
-                    c1 = corners[i];
-                    c2 = corners[j];
-                }
-            }
-        }
-
-        if (c1 != null)
-            angle = Math.atan2(c2.y - c1.y, c2.x - c1.x);
-
-        return smallestDistance;
+        return angle;
     }
 
     public static int getSidesCount(MatOfPoint contour) {
